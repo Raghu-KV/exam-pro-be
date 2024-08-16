048e9d67a9b6a5ea3f7ea0ff14e58a3e2dce4325
Raghu-KV <raghunandanv19@gmail.com>
2024-08-16 11:40:46 +0530

    changes for vercel

--- config/allowedOrgin.js
+++ config/allowedOrgin.js
@@ -1,1 +1,6 @@
-export const allowedOrgin = ["http://localhost:3000", "http://localhost:3001"];
+export const allowedOrgin = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://questive-student.netlify.app",
+  "https://questive-admin.netlify.app",
+];
